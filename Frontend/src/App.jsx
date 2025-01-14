f5a824ea3bb92c22672293597def9bd02b580b13
irshad1601 <mdirshadalam430@gmail.com>
2025-01-15 00:41:22 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import './index.css'
-import Signup from './pages/signup'
+import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import { Children, useState } from 'react'
