d40b9d4aacaec0fc2aeff4cffb2cfbb5ffa68643
irshad1601 <mdirshadalam430@gmail.com>
2025-02-16 18:55:51 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,14 +13,9 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use(
-  cors({
-    origin: "https://signup-login-system-op3h.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+  cors()
 );
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use("/auth",AuthRouter)
 app.use("/products",ProductRouter)
